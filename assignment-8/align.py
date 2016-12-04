c6707c5d134fe389683dde0949bae86c2e31c8db
Sergey Nikiforov <serge.a.nikiforov@gmail.com>
2016-12-04 16:30:49 +0300

    A8: add memoization

--- assignment-8/align.py
+++ assignment-8/align.py
@@ -4,14 +4,14 @@ import random  # for seed, random
 import sys  # for stdout
 
 
-def find_optimal_alignment(strand1, strand2):
+def find_optimal_alignment(dna1, dna2):
     """
     Computes the score of the optimal alignment of two DNA strands.
-    :param strand1: DNA strand 1 - str of A, C, G, T
-    :param strand2: DNA strand 2 - str of A, C, G, T
-    :return: dict with score of the optimal alignment and modified strands themselves
+    :param dna1: DNA strand 1 - str of A, C, G, T
+    :param dna2: DNA strand 2 - str of A, C, G, T
+    :return: tuple with score of the optimal alignment and modified strands themselves
     """
-    def find_optimal(strand1, strand2, memo=None):
+    def find_optimal(strand1, strand2, memo):
         """
         helper recursive function
         :param strand1: DNA strand 1 - str of A, C, G, T
@@ -21,21 +21,40 @@ def find_optimal_alignment(strand1, strand2):
         """
         # base case if one of the two strands is empty, then there is only
         # one possible alignment, and of course it's optimal
-        if len(strand1) == 0:
+        len1, len2 = len(strand1), len(strand2)
+        if len1 == 0:
             # score, strand1 leftover, strand2 leftover
-            return len(strand2) * -2, ' ' * len(strand2), strand2
-        if len(strand2) == 0:
+            result = len2 * -2, ' ' * len2, strand2
+
+            # remember call result
+            memo[(strand1, strand2)] = result
+            return result
+        if len2 == 0:
             # score, strand1 leftover, strand2 leftover
-            return len(strand1) * -2, strand1, ' ' * len(strand1)
+            result = len1 * -2, strand1, ' ' * len1
+
+            # remember call result
+            memo[(strand1, strand2)] = result
+            return result
 
         # There's the scenario where the two leading bases of
         # each strand are forced to align, regardless of whether or not
         # they actually match.
-        best_with, best_strand1, best_strand2 = find_optimal(strand1[1:], strand2[1:])
+
+        # first, try getting result from memo
+        try:
+            best_with, best_strand1, best_strand2 = memo[(strand1[1:], strand2[1:])]
+        except KeyError:
+            best_with, best_strand1, best_strand2 = find_optimal(strand1[1:], strand2[1:], memo)
+
         if strand1[0] == strand2[0]:
-            return (best_with + 1,
-                    strand1[0] + best_strand1,
-                    strand2[0] + best_strand2)  # no benefit from making other recursive calls
+            result = (best_with + 1,
+                      strand1[0] + best_strand1,
+                      strand2[0] + best_strand2)  # no benefit from making other recursive calls
+
+            # remember call result
+            memo[(strand1, strand2)] = result
+            return result
 
         best = best_with - 1
         best_strand1 = strand1[0] + best_strand1
@@ -43,7 +62,13 @@ def find_optimal_alignment(strand1, strand2):
 
         # It's possible that the leading base of strand1 best
         # matches not the leading base of strand2, but the one after it.
-        best_without, best_without_strand1, best_without_strand2 = find_optimal(strand1, strand2[1:])
+
+        # first, try getting result from memo
+        try:
+            best_without, best_without_strand1, best_without_strand2 = memo[(strand1, strand2[1:])]
+        except KeyError:
+            best_without, best_without_strand1, best_without_strand2 = find_optimal(strand1, strand2[1:], memo)
+
         best_without -= 2  # penalize for insertion of space
         if best_without > best:
             best_strand1 = ' ' + best_without_strand1
@@ -51,16 +76,26 @@ def find_optimal_alignment(strand1, strand2):
             best = best_without
 
         # opposite scenario
-        best_without, best_without_strand1, best_without_strand2 = find_optimal(strand1[1:], strand2)
+        # first, try getting result from memo
+        try:
+            best_without, best_without_strand1, best_without_strand2 = memo[(strand1[1:], strand2)]
+        except KeyError:
+            best_without, best_without_strand1, best_without_strand2 = find_optimal(strand1[1:], strand2, memo)
+
         best_without -= 2  # penalize for insertion of space
         if best_without > best:
             best_strand1 = strand1[0] + best_without_strand1
             best_strand2 = ' ' + best_without_strand2
             best = best_without
 
-        return best, best_strand1, best_strand2
+        result = best, best_strand1, best_strand2
 
-    return find_optimal(strand1, strand2)
+        # remember call result
+        memo[(strand1, strand2)] = result
+        return result
+
+    memo_dict = {}
+    return find_optimal(dna1, dna2, memo_dict)
 
 
 # Utility function that generates a random DNA string of
@@ -104,8 +139,8 @@ def main():
         answer = sys.stdin.readline()
         if answer == "no\n" or answer == "n\n":
             break
-        strand1 = generate_random_dna_strand(8, 10)
-        strand2 = generate_random_dna_strand(8, 10)
+        strand1 = generate_random_dna_strand(40, 60)
+        strand2 = generate_random_dna_strand(40, 60)
         print("Aligning these two strands: " + strand1)
         print("                            " + strand2)
         alignment = find_optimal_alignment(strand1, strand2)
